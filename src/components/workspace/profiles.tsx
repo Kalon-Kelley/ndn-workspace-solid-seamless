b4bcfdcbc160fea2cbbbe4e7c014460765f0a79f
Kalon Lucas Kelley <klucas.kelley@gmail.com>
2025-03-11 15:31:19 -0700

    Separated workspace creation and joining into new file
    * TODO: Currently the certs are just printed, I need to add text boxes for them

--- src/components/workspace/profiles.tsx
+++ src/components/workspace/profiles.tsx
@@ -19,6 +19,7 @@ import { Profile, toBootParams as profileToBootParams, profiles as db, createWor
 import { For, createEffect, createSignal } from 'solid-js'
 import { useNdnWorkspace } from '../../Context'
 import { base64ToBytes, bytesToBase64 } from '../../utils'
+import { CreateWorkspace, JoinWorkspace } from './seamless-workspace'
 import { Decoder, Encoder } from '@ndn/tlv'
 import { useNavigate } from '@solidjs/router'
 import { Certificate } from '@ndn/keychain'
@@ -27,12 +28,6 @@ import { Data } from '@ndn/packet'
 export default function Profiles() {
   const { booted, bootstrapWorkspace } = useNdnWorkspace()!
   const [profiles, setProfiles] = createSignal<Profile[]>([])
-  const [workspaceUri, setWorkspaceUri] = createSignal('')
-  const [workspaceProfile, setWorkspaceProfile] = createSignal('')
-  const [joinUsername, setJoinUsername] = createSignal('')
-  const [userPrvKeyBits, setUserPrvKeyBits] = createSignal<Uint8Array>()
-  const [trustAnchor, setTrustAnchor] = createSignal('')
-  const [cert, setCert] = createSignal('')
   
   const navigate = useNavigate()
 
@@ -65,183 +60,67 @@ export default function Profiles() {
     }
   }
 
-  const onCreateWorkspace = () => {
-    const uri = workspaceUri().trim()
-    const profile = workspaceProfile().trim()
-    if (uri) {
-      createWorkspace(uri, profile)
-        .then(() => db.loadAll())
-        .then((items) => setProfiles(items))
-    }
-  }
-
-  const onJoinWorkspace = () => {
-    console.log('JOINING WORKSPACE WITH USERNAME', joinUsername())
-    if (!userPrvKeyBits()) {
-      createUser(joinUsername())
-        .then(([sscert, pvtKey]) => {
-          setUserPrvKeyBits(pvtKey)
-          console.log('SS CERT', bytesToBase64(Encoder.encode(sscert.data)))
-        })
-    } else if (cert() && trustAnchor() && userPrvKeyBits()) {
-      const ta = Certificate.fromData(Decoder.decode(base64ToBytes(trustAnchor()), Data))
-      const cer = Certificate.fromData(Decoder.decode(base64ToBytes(cert()), Data))
-      addProfile(ta, userPrvKeyBits()!, cer)
-        .then(() => db.loadAll())
-        .then((items) => setProfiles(items))
-    }
-    // generate key for user and self signed cert
-    // output the crtificate as B64 so that the user can send it to the workspace admin
-    // Add a signal for the users prvKeyBits so that they can be accessed later
-    // In a new function when the user has their cert signed by the trust anchor and the trust anchor itself call profiles.save(fromBootParams({trustanchor, pkeybits, cert}))
-  }
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <Toolbar>
-          <Typography sx={{ flex: '1 1 100%' }} variant="h6" component="div">
-            Profiles
-          </Typography>
-          <Button onClick={() => navigate('/convert-testbed', { replace: true })} variant="outlined" color="secondary">
-            Convert
-          </Button>
-          <IconButton onClick={() => navigate('/workspace', { replace: true })}>
-            <PersonAddIcon color="primary" />
-          </IconButton>
-        </Toolbar>
-        <Box sx={{ display: "flex", gap: 2, alignItems: "stretch", p: 2 }}>
-          <Stack spacing={1} sx={{ flex: 1 }}>
-            <TextField
-              fullWidth
-              required
-              label="Username"
-              name="join-username"
-              type="text"
-              inputProps={{
-                style: {
-                  'font-family': '"Roboto Mono", ui-monospace, monospace',
-                  'white-space': 'pre',
-                },
-              }}
-              value={joinUsername()}
-              onChange={(event) => setJoinUsername(event.target.value)}
-            />
-            <TextField
-              fullWidth
-              required
-              label="Cert"
-              name="cert"
-              type="text"
-              inputProps={{
-                style: {
-                  'font-family': '"Roboto Mono", ui-monospace, monospace',
-                  'white-space': 'pre',
-                },
-              }}
-              value={cert()}
-              onChange={(event) => setCert(event.target.value)}
-            />
-            <TextField
-              fullWidth
-              required
-              label="Trust Anchor"
-              name="trust-anchor"
-              type="text"
-              inputProps={{
-                style: {
-                  'font-family': '"Roboto Mono", ui-monospace, monospace',
-                  'white-space': 'pre',
-                },
-              }}
-              value={trustAnchor()}
-              onChange={(event) => setTrustAnchor(event.target.value)}
-            />
-          </Stack>
-          <Button onClick={onJoinWorkspace} variant="contained" color="primary">
-            Join Workspace
-          </Button>
-        </Box>
-        <Box sx={{ display: "flex", gap: 2, alignItems: "stretch", p: 2 }}>
-          <Stack spacing={1} sx={{ flex: 1 }}>
-            <TextField
-              fullWidth
-              required
-              label="Workspace URI"
-              name="workspace-uri"
-              type="text"
-              inputProps={{
-                style: {
-                  'font-family': '"Roboto Mono", ui-monospace, monospace',
-                  'white-space': 'pre',
-                },
-              }}
-              value={workspaceUri()}
-              onChange={(event) => setWorkspaceUri(event.target.value)}
-            />
-            <TextField
-              fullWidth
-              required
-              label="Workspace Profile"
-              name="workspace-profile"
-              type="text"
-              inputProps={{
-                style: {
-                  'font-family': '"Roboto Mono", ui-monospace, monospace',
-                  'white-space': 'pre',
-                },
-              }}
-              value={workspaceProfile()}
-              onChange={(event) => setWorkspaceProfile(event.target.value)}
-            />
-          </Stack>
-          <Button onClick={onCreateWorkspace} variant="contained" color="primary">
-            Create Workspace
-          </Button>
-        </Box>
-        <TableContainer>
-          <Table sx={{ minWidth: 300 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell align="right">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <For each={profiles()}>
-                {(item, i) => (
-                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                    <TableCell component="th" scope="row">
-                      <Typography color="primary" fontFamily='"Roboto Mono", ui-monospace, monospace' component="span">
-                        {item.workspaceName}
-                      </Typography>
-                      <Typography fontFamily='"Roboto Mono", ui-monospace, monospace' component="span">
-                        {item.nodeId.substring(item.workspaceName.length)}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <IconButton
-                        onClick={() => {
-                          onRun(i())
-                        }}
-                      >
-                        <PlayArrowIcon color="success" />
-                      </IconButton>
-                      <IconButton
-                        onClick={() => {
-                          onRemove(i())
-                        }}
-                      >
-                        <DeleteIcon color="error" />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </For>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Box>
+    <Stack spacing={2}>
+      <CreateWorkspace setProfiles={setProfiles}/>
+      <JoinWorkspace setProfiles={setProfiles}/>
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ width: '100%', mb: 2 }}>
+          <Toolbar>
+            <Typography sx={{ flex: '1 1 100%' }} variant="h6" component="div">
+              Profiles
+            </Typography>
+            <Button onClick={() => navigate('/convert-testbed', { replace: true })} variant="outlined" color="secondary">
+              Convert
+            </Button>
+            <IconButton onClick={() => navigate('/workspace', { replace: true })}>
+              <PersonAddIcon color="primary" />
+            </IconButton>
+          </Toolbar>
+          <TableContainer>
+            <Table sx={{ minWidth: 300 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <For each={profiles()}>
+                  {(item, i) => (
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell component="th" scope="row">
+                        <Typography color="primary" fontFamily='"Roboto Mono", ui-monospace, monospace' component="span">
+                          {item.workspaceName}
+                        </Typography>
+                        <Typography fontFamily='"Roboto Mono", ui-monospace, monospace' component="span">
+                          {item.nodeId.substring(item.workspaceName.length)}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <IconButton
+                          onClick={() => {
+                            onRun(i())
+                          }}
+                        >
+                          <PlayArrowIcon color="success" />
+                        </IconButton>
+                        <IconButton
+                          onClick={() => {
+                            onRemove(i())
+                          }}
+                        >
+                          <DeleteIcon color="error" />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </For>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Box>
+    </Stack>
   )
 }
---
+++ src/components/workspace/seamless-workspace.tsx
@@ -0,0 +1,259 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  TextField,
+  IconButton,
+  Typography,
+  Button,
+  Divider,
+} from '@suid/material'
+import {
+  ExpandLess as ExpandLessIcon,
+  ExpandMore as ExpandMoreIcon,
+} from '@suid/icons-material'
+import { base64ToBytes, bytesToBase64 } from '../../utils'
+import { Decoder, Encoder } from '@ndn/tlv'
+import { Component, Data, Name, ValidityPeriod, type NameLike } from '@ndn/packet'
+import { Show, createEffect, createSignal } from 'solid-js'
+import { ECDSA, createSigner, createVerifier, Certificate, CertNaming, SigningAlgorithmListFull } from "@ndn/keychain"
+import { Profile, profiles as db, createWorkspace, createUser, addProfile } from '../../backend/models/profiles'
+
+export function CreateWorkspace(props: {
+  setProfiles: (value: Array<Profile> | undefined) => void
+}) {
+  const [workspaceName, setWorkspaceName] = createSignal('')
+  const [workspaceIdentity, setWorkspaceIdentity] = createSignal('')
+  const [expanded, setExpanded] = createSignal(false);
+
+  const onCreateWorkspace = () => {
+    const workspace = workspaceName().trim()
+    const identity = workspace.concat('/', workspaceIdentity().trim())
+    console.log('Creating a workspace', workspaceName(), 'With owner identity', identity)
+    createWorkspace(workspace, identity)
+      .then(() => db.loadAll())
+      .then((items) => props.setProfiles(items))
+  }
+
+  return (
+    <Card>
+      <CardHeader
+        sx={{ textAlign: "left" }}
+        title="Create a Workspace"
+        subheader={
+          <Typography color="primary" component={'span'}>
+            Enter a name for the Workspace and your identity in the workspace
+          </Typography>
+        }
+        action={
+          <IconButton onClick={() => setExpanded(!expanded())}>
+            <Show when={expanded()} fallback={<ExpandMoreIcon />}>
+              <ExpandLessIcon />
+            </Show>
+          </IconButton>
+        }
+      />
+      <Show when={expanded()}>
+        <Divider />
+        <CardContent>
+          <div
+            style={{
+              display: 'flex',
+              'flex-direction': 'column',
+              'align-items': 'flex-end',
+              'row-gap': '10px',
+            }}
+          >
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Workspace Name"
+              name="workspace-name"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={workspaceName()}
+              onChange={(event) => setWorkspaceName(event.target.value)}
+            />
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Workspace Identity"
+              name="workspace-identity"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={workspaceIdentity()}
+              onChange={(event) => setWorkspaceIdentity(event.target.value)}
+            />
+            <Button onClick={onCreateWorkspace} variant="contained">
+              Create Workspace
+            </Button>
+          </div>
+        </CardContent>
+      </Show>
+    </Card>
+  )
+}
+
+export function JoinWorkspace(props: {
+  setProfiles: (value: Array<Profile> | undefined) => void
+}) {
+  const [workspaceName, setWorkspaceName] = createSignal('')
+  const [workspaceIdentity, setWorkspaceIdentity] = createSignal('')
+  const [identityPrvKeyBits, setIdentityPrvKeyBits] = createSignal<Uint8Array>()
+  const [trustAnchor, setTrustAnchor] = createSignal('')
+  const [certificate, setCertificate] = createSignal('')
+  const [csrGenerated, setCsrGenerated] = createSignal(false);
+  const [expanded, setExpanded] = createSignal(false);
+  
+  const onGenerateCSR = () => {
+    const workspace = workspaceName().trim()
+    const identity = workspace.concat('/', workspaceIdentity().trim())
+    console.log('Creating a CSR for identity', identity)
+    createUser(identity)
+      .then(([sscert, pvtKey]) => {
+        setIdentityPrvKeyBits(pvtKey)
+        setCsrGenerated(true)
+        console.log('SS CERT\n', bytesToBase64(Encoder.encode(sscert.data)))
+      })
+  }
+
+  const onJoinWorkspace = () => {
+    console.log('Workapce Joined')
+    const trustAnchorCertificate = Certificate.fromData(Decoder.decode(base64ToBytes(trustAnchor()), Data))
+    const identityCertificate = Certificate.fromData(Decoder.decode(base64ToBytes(certificate()), Data))
+    addProfile(trustAnchorCertificate, identityPrvKeyBits()!, identityCertificate)
+      .then(() => db.loadAll())
+      .then((items) => props.setProfiles(items))
+  }
+  
+  return (
+    <Card>
+      <CardHeader
+        sx={{ textAlign: "left" }}
+        title="Join an existing Workspace"
+        subheader={
+          <Typography color="primary" component={'span'}>
+            Enter the name of the existing workspace and desired identity to generate a CSR, then send the CSR to the workspace owner for signing
+          </Typography>
+        }
+        action={
+          <IconButton onClick={() => setExpanded(!expanded())}>
+            <Show when={expanded()} fallback={<ExpandMoreIcon />}>
+              <ExpandLessIcon />
+            </Show>
+          </IconButton>
+        }
+      />
+      <Show when={expanded() && !csrGenerated()}>
+        <Divider />
+        <CardContent>
+          <div
+            style={{
+              display: 'flex',
+              'flex-direction': 'column',
+              'align-items': 'flex-end',
+              'row-gap': '10px',
+            }}
+          >
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Workspace Name"
+              name="workspace-name"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={workspaceName()}
+              onChange={(event) => setWorkspaceName(event.target.value)}
+            />
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Workspace Identity"
+              name="workspace-identity"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={workspaceIdentity()}
+              onChange={(event) => setWorkspaceIdentity(event.target.value)}
+            />
+            <Button onClick={onGenerateCSR} variant="contained">
+              Generate CSR
+            </Button>
+          </div>
+        </CardContent>
+      </Show>
+      <Show when={expanded() && csrGenerated()}>
+        <Divider />
+        <CardContent>
+          <div
+            style={{
+              display: 'flex',
+              'flex-direction': 'column',
+              'align-items': 'flex-end',
+              'row-gap': '10px',
+            }}
+          >
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Trust Anchor"
+              name="trust-anchor"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={trustAnchor()}
+              onChange={(event) => setTrustAnchor(event.target.value)}
+            />
+            <TextField
+              fullWidth
+              required
+              multiline
+              label="Certificate"
+              name="certificate"
+              type="text"
+              inputProps={{
+                style: {
+                  'font-family': '"Roboto Mono", ui-monospace, monospace',
+                  'white-space': 'pre',
+                },
+              }}
+              value={certificate()}
+              onChange={(event) => setCertificate(event.target.value)}
+            />
+            <Button onClick={onJoinWorkspace} variant="contained">
+              Join Workspace
+            </Button>
+          </div>
+        </CardContent>
+      </Show>
+    </Card>
+  )
+}
